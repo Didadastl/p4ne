0d726a7145adb56ddabad28019a4947682898279
ds.kiselev <kiselevd1991@gmail.com>
2023-03-24 16:41:22 +0300

    ready

--- lab1_5/lab1_5.py
+++ lab1_5/lab1_5.py
@@ -5,7 +5,7 @@ for file in list_files:
     with open(file, 'r') as f:
         for lines in f:
             if 'ip address' in lines and 'no' not in lines:
-                list_of_ip_add.append(lines.replace('\n', '')[1::])
+                list_of_ip_add.append(lines.replace('\n', '').replace(' ip address ', ''))
 print(set(list_of_ip_add))
 
 
